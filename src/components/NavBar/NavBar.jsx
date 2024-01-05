39476b232ad866d9973854c94b8009785d461777
OATheSWE <osborneosas12@gmail.com>
2024-01-05 14:19:50 +0100

    Added hover to the button

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -178,7 +178,7 @@ export default function NavBar() {
           >
             <Btn
               text="Hire me"
-              style={`bg-white text-black w-[92vw] max-[360px]:w-[88vw] rounded-3xl border-2 hover:border-accent border-solid hover:bg-accent hover:text-white mt-8`}
+              style={`bg-white text-black w-[92vw] max-[360px]:w-[88vw] rounded-3xl border-2 hover:border-accent border-solid hover:bg-accent hover:text-white mt-8 cursor-pointer`}
             />
           </a>
         </ScrollArea>
